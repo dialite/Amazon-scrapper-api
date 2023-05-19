9b94a3b076543c4c2c5bb60727b818cacca16d5d
dialite <raymondolisa01@gmail.com>
2023-05-19 10:02:21 +0100

    Initialize use, get, and list for app

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require("express");
+const res = require("express/lib/response");
 const request = require("request-promise");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcom to Amazon API scraper");
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

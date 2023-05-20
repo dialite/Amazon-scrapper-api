0dabc3adb8e7dd5bd4fb1fb160012b669c943b19
dialite <raymondolisa01@gmail.com>
2023-05-20 12:59:05 +0100

    Create response to get product offers

--- index.js
+++ index.js
@@ -43,4 +43,18 @@ app.get("/product/:productId/reviews", async (req, res) => {
   }
 });
 
+// GET PRODUCT OFFERS
+app.get("/product/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseURL}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
